4de932237f692a180d7152f714ab849b7b4b3d62
claudiadel <cdelbarr@gmail.com>
2018-02-15 14:48:39 +0100

    14:48

--- inc/nav.php
+++ inc/nav.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  			<a class="navbar-brand" href="index.html">
+	  			<a class="navbar-brand" href="index.php">
 	  					<span class="navbar-brand mb-0 h1">*Inicio*</span>
 	  			</a>
 	  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
