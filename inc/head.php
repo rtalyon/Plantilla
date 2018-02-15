c23d17542a526879ead8136745475b96f07a7fae
rafatbd <rafatinfo4@gmail.com>
2018-02-15 14:59:04 +0100

    head.php

--- inc/head.php
+++ inc/head.php
@@ -2,11 +2,11 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<link rel="stylesheet" type="text/css" href="../css/style.css">
 	</head>
 	<body>
-		
 		<?php include("header.php");?>
