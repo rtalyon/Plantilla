ade10c22f353e72852fa146cd05fb5c7d47465c0
belendiaz0808 <belendiazsoler0808@gmail.com>
2018-02-15 14:47:53 +0100

    index estructura

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php include("inc/head.php");?>
-	<div id="content" class="container-fluid"></div>
+	<div id="content" class="container-fluid">
+		<div class="row">			
+		</div>
+	</div>
 <?php include("inc/footer.php");?>
