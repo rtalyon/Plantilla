b975ea592fe260d7f84acf35f74525d6dbd0a7b0
claudiadel <cdelbarr@gmail.com>
2018-02-15 14:19:04 +0100

    Nav bar

---
+++ inc/nav.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  			<a class="navbar-brand" href="index.html">
+	  				<span class="fab fa-fort-awesome-alt"></span>
+	  					<span class="navbar-brand mb-0 h1">*Inicio*</span>
+	  			</a>
+	  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    			<span class="navbar-toggler-icon"></span>
+	  			</button>
+	  			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    			<ul class="navbar-nav mr-auto">
+	      				<li class="nav-item dropdown">
+	        				<a class="nav-link active dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Desplegable 1</a>
+	        					<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	        					  <a class="dropdown-item" href="ofertas.html" target="_blank">Info 1</a>
+	        					   <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					   <a class="dropdown-item" href="ofertas.html" target="_blank">Info 2</a>
+	        					    <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					  <a class="dropdown-item" href="ofertas.html" target="_blank">Info 3</a>
+	        					   <div class="dropdown-divider"></div> <!-- DIVIDER -->
+	        					</div>
+	      				</li>
+	      		<li class="nav-item active">
+	        		<a class="nav-link" href="categorias.html" target="_blank">Contenido 2</a>
+	      		</li>
+	      		<li class="nav-item active">
+	        		<a class="nav-link" href="establecimientos.html">Contenido 3</a>
+	      		</li>
+	    			</ul>
+	  			</div>
+</nav>
